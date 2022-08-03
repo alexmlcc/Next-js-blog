713ec247e543bba3b56e7a7830fdaedac16cf069
alexmlcc <110511624+alexmlcc@users.noreply.github.com>
2022-08-02 20:46:08 -0700

    Update Head.tsx

--- components/Head.tsx
+++ components/Head.tsx
@@ -8,9 +8,9 @@ export const WEBSITE_HOST_URL = 'https://nextjs-typescript-mdx-blog.vercel.app';
 const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
   const router = useRouter();
   const meta: MetaProps = {
-    title: 'Hunter Chang - Website',
+    title: 'Travel Tapes',
     description:
-      'Sleep Deprived Father. Senior Web Developer. Lover of all things Ramen and Kpop.',
+      'Travel Blog',
     image: `${WEBSITE_HOST_URL}/images/site-preview.png`,
     type: 'website',
     ...customMeta,
